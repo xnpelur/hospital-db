9c617f168f3c6e3bcfae35b7325c615d69be2470
xnpelur <michael.s5102@gmail.com>
2024-04-17 14:25:14 +0300

    More data validation triggers

--- db/03 - triggers.sql
+++ db/03 - triggers.sql
@@ -41,3 +41,100 @@ CREATE TRIGGER update_patient_record_discharge_date_trigger
 AFTER INSERT OR UPDATE ON treatment_record
 FOR EACH ROW
 EXECUTE FUNCTION update_patient_record_discharge_date();
+
+CREATE OR REPLACE FUNCTION check_patient_record_discharge_date()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    max_end_date DATE;
+BEGIN
+    IF NEW.discharge_date < NEW.admission_date THEN
+        RAISE EXCEPTION 'Discharge date cannot be less than admission date for the patient record.';
+    END IF;
+
+    SELECT MAX(end_date)
+    INTO max_end_date
+    FROM treatment_record
+    WHERE clinical_record_id IN (
+        SELECT id
+        FROM clinical_record
+        WHERE patient_record_id = NEW.id
+    );
+
+    IF NEW.discharge_date < max_end_date THEN
+        RAISE EXCEPTION 'Discharge date cannot be less than the maximum treatment end date for the patient record.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER check_patient_record_discharge_date_trigger
+BEFORE UPDATE ON patient_record
+FOR EACH ROW
+EXECUTE FUNCTION check_patient_record_discharge_date();
+
+CREATE OR REPLACE FUNCTION check_treatment_record_start_date()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    patient_admission_date DATE;
+BEGIN
+    SELECT admission_date
+    INTO patient_admission_date
+    FROM patient_record
+    WHERE id = (
+        SELECT patient_record_id
+        FROM clinical_record
+        WHERE id = NEW.clinical_record_id
+    );
+
+    IF NEW.start_date < patient_admission_date THEN
+        RAISE EXCEPTION 'Treatment start date cannot be earlier than the patient admission date.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER check_treatment_record_start_date_trigger
+BEFORE INSERT OR UPDATE ON treatment_record
+FOR EACH ROW
+EXECUTE FUNCTION check_treatment_record_start_date();
+
+CREATE OR REPLACE FUNCTION check_patient_record_admission_date()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    patient_birth_date DATE;
+    doctor_enrollment_date DATE;
+BEGIN
+    SELECT birth_date
+    INTO patient_birth_date
+    FROM patient
+    WHERE id = NEW.patient_id;
+
+    SELECT enrollment_date
+    INTO doctor_enrollment_date
+    FROM doctor
+    WHERE id = NEW.doctor_id;
+
+    IF NEW.admission_date < patient_birth_date THEN
+        RAISE EXCEPTION 'Admission date cannot be earlier than the patient birth date.';
+    END IF;
+
+    IF NEW.admission_date < doctor_enrollment_date THEN
+        RAISE EXCEPTION 'Admission date cannot be earlier than the doctor enrollment date.';
+    END IF;
+
+    RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER check_patient_record_admission_date_trigger
+BEFORE INSERT OR UPDATE ON patient_record
+FOR EACH ROW
+EXECUTE FUNCTION check_patient_record_admission_date();
