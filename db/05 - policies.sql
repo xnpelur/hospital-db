8e658d06b30a7bd0d09725650974ca012c0d1a46
xnpelur <michael.s5102@gmail.com>
2024-04-23 11:13:55 +0300

    Doctors only allowed to select their own patient_records

--- db/05 - policies.sql
+++ db/05 - policies.sql
@@ -2,11 +2,16 @@ CREATE POLICY doctor_select ON social_status FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON patient FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON department FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON doctor FOR SELECT TO doctor USING (true);
-CREATE POLICY doctor_select ON patient_record FOR SELECT TO doctor USING (true);
+CREATE POLICY doctor_select ON patient_record TO doctor USING (EXISTS (
+    SELECT 1
+    FROM doctor
+    WHERE doctor.id = patient_record.doctor_id
+        AND doctor.username = current_user
+));
 CREATE POLICY doctor_select ON disease FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON treatment FOR SELECT TO doctor USING (true);
-CREATE POLICY doctor_select ON clinical_record FOR SELECT TO doctor USING (true);
-CREATE POLICY doctor_select ON treatment_record FOR SELECT TO doctor USING (true);
+CREATE POLICY doctor_select ON clinical_record TO doctor USING (true);
+CREATE POLICY doctor_select ON treatment_record TO doctor USING (true);
 
 CREATE POLICY admin_select ON social_status FOR SELECT TO admin USING (true);
 CREATE POLICY admin_select ON patient FOR SELECT TO admin USING (true);
