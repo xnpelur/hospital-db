69c3be6d9496c103f4218c414815d353f9000ab4
xnpelur <michael.s5102@gmail.com>
2024-05-07 16:55:48 +0300

    Doctor's RLS policies finished

--- db/06 - policies.sql
+++ db/06 - policies.sql
@@ -1,7 +1,7 @@
 CREATE POLICY doctor_select ON social_status FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON patient FOR SELECT TO doctor USING (true);
 CREATE POLICY doctor_select ON department FOR SELECT TO doctor USING (true);
-CREATE POLICY doctor_select ON doctor FOR SELECT TO doctor USING (true);
+CREATE POLICY doctor_select ON doctor FOR SELECT TO doctor USING (username = current_user);
 CREATE POLICY doctor_all ON patient_record TO doctor USING (EXISTS (
     SELECT 1
     FROM doctor
@@ -20,17 +20,19 @@ CREATE POLICY doctor_all ON clinical_record TO doctor USING (EXISTS (
         WHERE doctor.username = current_user
     )
 ));
-CREATE POLICY doctor_select ON treatment_record TO doctor USING (true);
-
-CREATE POLICY admin_select ON social_status FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON patient FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON department FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON doctor FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON patient_record FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON disease FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON treatment FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON clinical_record FOR SELECT TO admin USING (true);
-CREATE POLICY admin_select ON treatment_record FOR SELECT TO admin USING (true);
+CREATE POLICY doctor_all ON treatment_record TO doctor USING (
+    EXISTS (
+        SELECT 1
+        FROM clinical_record
+        JOIN patient_record ON clinical_record.patient_record_id = patient_record.id
+        WHERE treatment_record.clinical_record_id = clinical_record.id
+        AND patient_record.doctor_id = (
+            SELECT id
+            FROM doctor
+            WHERE doctor.username = current_user
+        )
+    )
+);
 
 CREATE POLICY patient_select ON social_status FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON patient FOR SELECT TO patient USING (true);
@@ -41,3 +43,13 @@ CREATE POLICY patient_select ON disease FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON treatment FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON clinical_record FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON treatment_record FOR SELECT TO patient USING (true);
+
+CREATE POLICY admin_select ON social_status FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON patient FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON department FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON doctor FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON patient_record FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON disease FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON treatment FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON clinical_record FOR SELECT TO admin USING (true);
+CREATE POLICY admin_select ON treatment_record FOR SELECT TO admin USING (true);
