dba4d208211181454d61c1d82e049c2da32f5b3a
xnpelur <michael.s5102@gmail.com>
2024-05-07 17:02:26 +0300

    Patient's RLS policies finished

--- db/06 - policies.sql
+++ db/06 - policies.sql
@@ -20,29 +20,51 @@ CREATE POLICY doctor_all ON clinical_record TO doctor USING (EXISTS (
         WHERE doctor.username = current_user
     )
 ));
-CREATE POLICY doctor_all ON treatment_record TO doctor USING (
-    EXISTS (
-        SELECT 1
-        FROM clinical_record
-        JOIN patient_record ON clinical_record.patient_record_id = patient_record.id
-        WHERE treatment_record.clinical_record_id = clinical_record.id
-        AND patient_record.doctor_id = (
-            SELECT id
-            FROM doctor
-            WHERE doctor.username = current_user
-        )
+CREATE POLICY doctor_all ON treatment_record TO doctor USING (EXISTS (
+    SELECT 1
+    FROM clinical_record
+    JOIN patient_record ON clinical_record.patient_record_id = patient_record.id
+    WHERE treatment_record.clinical_record_id = clinical_record.id
+    AND patient_record.doctor_id = (
+        SELECT id
+        FROM doctor
+        WHERE doctor.username = current_user
     )
-);
+));
 
 CREATE POLICY patient_select ON social_status FOR SELECT TO patient USING (true);
-CREATE POLICY patient_select ON patient FOR SELECT TO patient USING (true);
+CREATE POLICY patient_select ON patient FOR SELECT TO patient USING (username = current_user);
 CREATE POLICY patient_select ON department FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON doctor FOR SELECT TO patient USING (true);
-CREATE POLICY patient_select ON patient_record FOR SELECT TO patient USING (true);
+CREATE POLICY patient_select ON patient_record FOR SELECT TO patient USING (EXISTS (
+    SELECT 1
+    FROM patient
+    WHERE patient.id = patient_record.patient_id
+        AND patient.username = current_user
+));
 CREATE POLICY patient_select ON disease FOR SELECT TO patient USING (true);
 CREATE POLICY patient_select ON treatment FOR SELECT TO patient USING (true);
-CREATE POLICY patient_select ON clinical_record FOR SELECT TO patient USING (true);
-CREATE POLICY patient_select ON treatment_record FOR SELECT TO patient USING (true);
+CREATE POLICY patient_select ON clinical_record FOR SELECT TO patient USING (EXISTS (
+    SELECT 1
+    FROM patient_record
+    WHERE patient_record.id = clinical_record.patient_record_id
+    AND patient_record.patient_id = (
+        SELECT id
+        FROM patient
+        WHERE patient.username = current_user
+    )
+));
+CREATE POLICY patient_select ON treatment_record FOR SELECT TO patient USING (EXISTS (
+    SELECT 1
+    FROM clinical_record
+    JOIN patient_record ON clinical_record.patient_record_id = patient_record.id
+    WHERE treatment_record.clinical_record_id = clinical_record.id
+    AND patient_record.patient_id = (
+        SELECT id
+        FROM patient
+        WHERE patient.username = current_user
+    )
+));
 
 CREATE POLICY admin_select ON social_status FOR SELECT TO admin USING (true);
 CREATE POLICY admin_select ON patient FOR SELECT TO admin USING (true);
