847082e7be060898be04f470169fdb18f94c0e07
xnpelur <michael.s5102@gmail.com>
2024-05-22 21:25:11 +0300

    Fix styling in head doctor page

--- app/(app)/headDoctorPage.tsx
+++ app/(app)/headDoctorPage.tsx
@@ -6,9 +6,9 @@ export default async function HeadDoctorPage() {
     const queries = await getTableValues<Query>("query");
 
     return (
-        <div className="flex h-full flex-col space-y-8">
+        <div className="flex h-full max-h-[calc(80vh-100px)] flex-col space-y-8">
             <h1 className="text-3xl font-bold">Отчёты</h1>
-            <div className="grid gap-6">
+            <div className="grid gap-6 overflow-y-auto pr-3">
                 {queries.map((query, index) => (
                     <HeadDoctorPageCard
                         key={index}
